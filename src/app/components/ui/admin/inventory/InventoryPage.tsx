b94fe40b792e8822ccccf22f4a796ebbf50732e6
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-05-04 21:37:03 +0000

    did a thing about the modal

--- src/app/components/ui/admin/inventory/InventoryPage.tsx
+++ src/app/components/ui/admin/inventory/InventoryPage.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
-//import AddProductModal from "./AddProductModal";
+import AddProductModal from "./AddProductModal";
 
 const InventoryPage = () => {
   const [showModal, setShowModal] = useState(false);
@@ -116,14 +116,13 @@ const InventoryPage = () => {
       </div>
 
       {showModal && (
-        // <AddProductModal
-        //   setShowModal={setShowModal}
-        //   animateModal={animateModal}
-        //   setAnimateModal={setAnimateModal}
-        //   editing={editing}
-        //   setEditing={setEditing}
-        // />
-        <div>hi</div>
+        <AddProductModal
+          setShowModal={setShowModal}
+          animateModal={animateModal}
+          setAnimateModal={setAnimateModal}
+          editing={editing}
+          setEditing={setEditing}
+        />
       )}
     </div>
   );
